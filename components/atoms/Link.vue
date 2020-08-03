5f1a6ea6a264ad609e36ac13a491b0c044c7327f
清水園子 <s-shimizu@tsumikiinc.com>
2020-08-04 01:11:24 +0900

    テキストリンク用コンポーネント作成

---
+++ components/atoms/Link.vue
@@ -0,0 +1,22 @@
+<template lang="pug">
+  nuxt-router.link(
+    to = "to"
+    )
+    |{{ label }}
+</template>
+<script>
+export default {
+  props: {
+    to: {
+      type: String,
+      required: true
+    }
+  }
+};
+</script>
+<style lang="scss" scoped>
+.link {
+  cursor: pointer;
+  color: #fff;
+}
+</style>
