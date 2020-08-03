72361d1d42fd43cb5cc6d18ff918a811a2587370
清水園子 <s-shimizu@tsumikiinc.com>
2020-08-03 18:54:35 +0900

    ログインページ仮追加

---
+++ pages/login.vue
@@ -0,0 +1,34 @@
+<template lang="pug">
+.container
+  div
+    h1.title
+      |login
+    div ここにgoogle用リンクを追加
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: "LOGIN"
+    };
+  },
+  head() {
+    return {
+      title: this.title,
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content: "login page"
+        },
+        {
+          hid: "og:type",
+          property: "og:type",
+          content: "article"
+        }
+      ]
+    };
+  }
+};
+</script>
