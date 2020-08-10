2730e99a2eb9545e51e92fdab10e4fbfd6c5a300
清水園子 <s-shimizu@tsumikiinc.com>
2020-08-10 23:26:20 +0900

    記事データ取得

--- pages/_slug/_id.vue
+++ pages/_slug/_id.vue
@@ -1,14 +1,65 @@
 <template lang="pug">
 .blog
   h1.title
-    |{{ $store.state.blog.data.title }}
-  .data(
-    v-html="$md.render($store.state.blog.data.description)"
+    |{{ post.title }}
+  .desc(
+    v-html = "$md.render(post.description)"
     )
 </template>
-<script></script>
+<script>
+import { createClient } from "~/plugins/contentful.js";
+const client = createClient();
+export default {
+  asyncData({ env, route }) {
+    return Promise.all([
+      // fetch all blog posts sorted by creation date
+      client.getEntries({
+        content_type: env.CTF_BLOG_POST_TYPE_ID,
+        "fields.slug[match]": route.params.id,
+        order: "-sys.createdAt"
+      })
+    ])
+      .then((post) => {
+        // return data that should be available
+        // in the template
+        return {
+          post: post[0].items[0].fields
+        };
+      })
+      .catch(console.error);
+  },
+  methods: {
+    setData(data) {
+      this.$store.commit("blog/setBlogData", data);
+    }
+  },
+  head() {
+    return {
+      title: this.post.title,
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content: "My custom description2"
+        },
+        {
+          hid: "og:type",
+          property: "og:type",
+          content: "article"
+        }
+      ]
+    };
+  }
+};
+</script>
 <style lang="scss" scoped>
 .blog {
-  text-align: left;
+  @include l-mobile {
+    padding-top: 40px;
+  }
+  &__desc {
+    @include fontSizeAll(24, 24, 24);
+    color: $color-text;
+  }
 }
 </style>
